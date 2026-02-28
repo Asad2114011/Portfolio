e45fc97e496b7798e6f82b47eb6b3e519fcaaaa3
Asad2114011 <asad2114011@gmail.com>
2026-02-28 12:51:16 +0600

    add health checker

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from cp.models import cp
 from project.models import project,Tag
 from blog.models import blog
+from django.http import JsonResponse
+from django.db import connection
 
 def home(request):
     cp_details=cp.objects.all()
@@ -9,3 +11,8 @@ def home(request):
     blogs=blog.objects.all()
     project_tag=Tag.objects.filter(projects__isnull=False).distinct()
     return render(request,'home.html',{'cp_details':cp_details,'projects':projects,'tag':project_tag,'blogs':blogs})
+
+def health(request):
+    with connection.cursor()as cursor:
+        cursor.execute("SELECT 1")
+    return JsonResponse({'status':'ok'})
